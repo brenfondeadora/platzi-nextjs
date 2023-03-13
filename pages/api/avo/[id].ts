b1b6d604f157e2d50aa4d48b1eab180bcc861bd4
Brenda SC <brenda.saavedra@fondeadora.com>
2023-03-13 14:15:29 -0600

    feat: add functionality to return an avocado by id

--- pages/api/avo/[id].ts
+++ pages/api/avo/[id].ts
@@ -1,14 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import DB from '@database'
 
-const allAvos = async (request: NextApiRequest, response: NextApiResponse) => {
+const anAvo = async (request: NextApiRequest, response: NextApiResponse) => {
   const db = new DB()
   const id = request.query.id
   const item = await db.getById(id as string)
 
-  response.statusCode = 200
-  response.setHeader('Content-type', 'application/json')
-  response.end(JSON.stringify({ data: item }))
+  response.status(200).json({ item })
 }
 
-export default allAvos
+export default anAvo
