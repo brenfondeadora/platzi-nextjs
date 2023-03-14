dffff3fecb13dd1e865e5a3e3be41284cce44b69
Brenda SC <brenda.saavedra@fondeadora.com>
2023-03-14 13:18:55 -0600

    style: add stylejs

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -7,6 +7,13 @@ const Layout: React.FC = ({ children }) => {
       <Navbar />
       {children}
       <footer>This is the footer</footer>
+      <style jsx>
+        {`
+          div {
+            background: salmon;
+          }
+        `}
+      </style>
     </div>
   )
 }
